af69d826fdee584f375aaab2bceea3ac758631b8
hithaankam <23wh1a1289@bvrithyderabad.edu.in>
2025-03-01 10:27:56 +0530

    Removed cached files

--- utils.py
+++
@@ -1,9 +0,0 @@
-import huggingface_hub
-
-def validate_token(token: str) -> bool:
-    """Validate Hugging Face token."""
-    try:
-        huggingface_hub.HfApi().whoami(token=token)
-        return True
-    except Exception:
-        return False
